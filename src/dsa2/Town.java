5200272970784547b7a972d3c8869912fea3905d
fritz <fritz@192.168.0.241>
2018-07-24 22:31:00 +0100

    Town (vertex) object class

--- src/dsa2/Town.java
+++ src/dsa2/Town.java
@@ -1,5 +1,13 @@
 package dsa2;
 
 public class Town {
-//
+	private String name;
+	private boolean wasChecked;
+	
+	public Town(String name)
+	{
+		this.name = name;
+		this.wasChecked = false;
+	}
+
 }
